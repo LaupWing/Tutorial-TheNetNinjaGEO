ecbcdc819adbda744456339fef2a957ba5930350
LaupWing <loc_strict@hotmail.com>
2019-08-20 14:23:36 +0200

    added comments

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -1,22 +1,92 @@
 <template>
     <div class="view-profile container">
-        <div class="card">
-            <h2 class="deep-purple-text center">User's Wall</h2>
+        <div class="card" v-if="profile">
+            <h2 class="deep-purple-text center">{{profile.alias}}</h2>
+            <ul class="comments collection">
+                <li 
+                    v-for="(comment,index) in comments" 
+                    :key="index">
+                    <div class="deep-purple-text">{{comment.from}}</div>
+                    <div class="grey-text text-darken-2">{{comment.content}}</div>    
+                </li>
+            </ul>
+            <form @submit.prevent="addComment">
+                <div class="field">
+                    <label for="comment">Add A Comment</label>
+                    <input type="text" name="comment" v-model="newComment">
+                    <p class="red-text center" v-if="feedback">{{feedback}}</p>
+                </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script>
 import db from '@/firebase/init'
+import firebase from 'firebase'
 export default {
     name: 'ViewProfiel',
     data(){
         return{
-            profile:null
+            profile:null,
+            newComment: null,
+            feedback: null,
+            user: null,
+            comments: []
+        }
+    },
+    methods:{
+        addComment(){
+            if(this.newComment){
+                this.feedback = null
+                
+                db.collection('comments').add({
+                    to: this.$route.params.id,
+                    from: this.user.id,
+                    content: this.newComment,
+                    time: Date.now()
+                })
+                .then(()=>{
+                    this.newComment = null
+                })
+            }
+            else{
+                this.feedback = 'You must enter a comment to add it'
+            }
         }
     },
     created(){
-        let ref = db.collection('users')
+
+        let ref = db.collection('user')
+        
+        ref.doc(this.$route.params.id).get()
+            .then(user=>{
+                this.profile = user.data()
+            })
+        // Get current user
+        ref
+            .where('user_id', '==', firebase.auth().currentUser.uid)
+            .get()
+            .then(snapshot=>{
+                snapshot.forEach(doc=>{
+                    this.user = doc.data()
+                    this.user.id = doc.id
+                })
+            })
+        db.collection('comments')
+            .where('to','==', this.$route.params.id)
+            .onSnapshot((snapshot)=>{
+                snapshot.docChanges().forEach((change)=>{
+                    console.log(change)
+                    if(change.type === 'added'){
+                        this.comments.unshift({
+                            from: change.doc.data().from,
+                            content: change.doc.data().content
+                        })
+                    }
+                })
+                console.log(this.comments)
+            })
     }
 }
 </script>
